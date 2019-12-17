2e62ec9e58aa96d5c1a90543dcd79a1dc764d36a
wyx <313835075@qq.com>
2019-12-17 16:04:16 +0800

    wyx

--- test.c
+++ test.c
@@ -1,11 +1,4 @@
-/*大小端转换函数*/
-/*函数功能： 将输入的 origindata 进行大小端转换*/
-/*要求：
-  1.通过代码阅读，清楚该函数的作用
-  2.该函数存在明显 bug， 请找出并修复
-  3.自建一个 origindata，修复 bug 后运行输出结果*/
-
-int rawdata_to_little_endian(uint8_t *origindata,
+﻿int rawdata_to_little_endian(uint8_t *origindata,
         uint32_t row_num, uint32_t row_size) {
     gf_error_t err = GF_SUCCESS;
     uint32_t row = 0;
@@ -38,6 +31,5 @@ int rawdata_to_little_endian(uint8_t *origindata,
             }
         }
     } while (0);
-
     return 0;
 }
